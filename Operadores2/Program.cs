76a8cd5e47fcce3bc5b233c22061e8a763d62cbe
João Vítor <Joo Vtor@DESKTOP-1O6R9P5>
2023-08-18 00:12:03 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores2
+{
+    internal class Program
+    {
+               #region teste calc
+                //Console.WriteLine("Entre com o primeiro número: ");
+                //double num1 = double.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Entre com o segundo número: ");
+                //double num2 = double.Parse(Console.ReadLine());
+
+                //double resultado = num1 + num2;
+
+                //Console.WriteLine("Resultado: " + resultado);
+                //Console.ReadKey();
+                #endregion
+
+                #region Operadores Matemáticos
+                //int num1 = 15;
+                //int num2 = 2;
+                //int resultado = num1 + num2;
+
+                //Console.WriteLine(num1 + num2);
+                //Console.WriteLine(num1 - num2);
+                //Console.WriteLine(num1 * num2);
+                //Console.WriteLine(num1 / num2);
+                //Console.WriteLine(num1 % num2);
+                #endregion
+
+                #region Procedencia de Operadores
+                //double num1 = 100;
+                //double num2 = 10;
+                //double num3 = 5;
+
+                //double res = (num1 + num2) * (num3 / 3);
+
+                //Console.WriteLine(res);
+                //Console.ReadKey();
+                #endregion
+
+                #region Incremento e Decremento
+                //int num1 = 10;
+
+                //num1 = num1 + 1;
+                //num1++;
+                //++num1;
+                //num1--;
+                //--num1;
+
+                //Console.WriteLine(--num1);
+                //Console.WriteLine(num1);
+
+                //Console.ReadKey();
+                #endregion
+
+                #region Concatenação
+                //string nome = "Gabriel";
+                //string sobrenome = "Artigas";
+                //string nomecompleto = nome + sobrenome + " " + 1985;
+
+                //Console.WriteLine(nomecompleto);
+                //Console.ReadKey();
+                #endregion
+
+                #region Operadores de Atribuição
+                //int num1 = 13;
+
+                //num1 = num1 + 20;
+                //num1 += 20;
+                //num1 -= 2;
+                //num1 *= 3;
+                //num1 /= 2;
+                //num1 %= 2;
+
+                //string nome = "Gabriel";
+                //nome += " Artigas";
+
+                //Console.WriteLine(nome);
+                //Console.ReadKey();  
+                #endregion
+
+                #region Igualdade / Copmparação 
+                //bool res = 100 == (50 * 2);
+                //bool res2 = (200 / 2) != (100 + 100);
+
+                //string nome = "Gabriel";
+                //bool res3 = nome != "Lucas";
+
+                //Console.WriteLine(res3);
+                //Console.ReadKey();  
+                #endregion
+
+                #region Relacionais
+                //bool res = 100 < 50 * 2;
+                //bool res = 100 + 25 > 50 * 2;
+                //bool res = 100 <= 50 * 2;
+                //bool res = 75 >= 50;
+
+                //Console.WriteLine(res);
+                //Console.ReadKey();  
+                #endregion
+
+                #region Operadores Lógicos
+                //bool res1 = 100 >= 120;
+                //bool res2 = 60 == 50;
+
+                //bool final = res1 && res2;
+                //bool final2 = (100 >= 50) || (60 == 50);
+
+                //Console.WriteLine(final2);
+                //Console.ReadKey();
+                #endregion
+
+
+            }
+        }
+    }
+}
